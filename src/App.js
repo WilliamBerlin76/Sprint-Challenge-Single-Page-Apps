418ee169d202d5318ff8feb8387dff1f9ab60fc6
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-20 10:50:45 -0500

    router and styled components complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Header from "./components/Header.js";
-import CharacterList from "./components/CharacterList"
+import CharacterList from "./components/CharacterList";
+import styled from "styled-components"
+import { Route, Link } from 'react-router-dom';
+import WelcomePage from "./components/WelcomePage.js";
 
 export default function App() {
+
+  const Links = styled.div`
+    text-align: center;
+    display: flex;
+    justify-content: space-evenly
+  `
   return (
     <main>
       <Header />
-      <CharacterList/>
+      <Links>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/CharacterList'}>See Characters</Link>
+      </Links>
+      <Route exact path='/' component={WelcomePage}/>
+      <Route path='/CharacterList' component={CharacterList} />
     </main>
   );
 }
